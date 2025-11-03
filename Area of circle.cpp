cb774b47925d8d9275b4e5b14fe266868e0ab994
parimisrinagalakshmi-dot <parimisrinagalakshmi@gmail.com>
2025-11-03 15:31:43 +0530

    Add files via upload

---
+++ Area of circle.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int l,b,area;
+  printf("enter l value");
+  scanf("%d",&l);
+  printf("enter b value");
+  scanf("%d",b);
+   area= l*b;
+  printf("area of rectangle=%d",area);
+  return 0;
+  
+}
+  
