fedebdd21ab440e7a78eb40fdbb7df357c48cc6f
parimisrinagalakshmi-dot <parimisrinagalakshmi@gmail.com>
2025-11-18 15:44:28 +0530

    Add files via upload

---
+++ Area of triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  float b,h,area;
+  printf("enter b value");
+  scanf("%f",&b);
+  printf("enter h value");
+  scanf("%f",&h);
+   area= b*h/2;
+  printf("area of triangle=%f",area);
+  return 0;
+}
+  
