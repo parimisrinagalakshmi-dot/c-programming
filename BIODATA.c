2123b50d81ea6570e4b3cf0596904f120fc9ff2f
parimisrinagalakshmi-dot <parimisrinagalakshmi@gmail.com>
2025-11-25 15:05:15 +0530

    Add files via upload

---
+++ BIODATA.c
@@ -0,0 +1,12 @@
+//BIODATA
+#include<stdio.h>
+int main()
+{
+	printf("Name:: P.Nagalakshmi");
+	printf("DOB:: 09-11-2007");
+	printf("PLACE:: Komaragiri");
+	printf("FATHER NAME:: P.Rama Satyam");
+	printf("MOTHER NAME:: P.Veeramani");
+	printf("SISTER NAME:: P.Sandhya Devi");
+	return 0;
+}
